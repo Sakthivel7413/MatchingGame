0d092db849e8499f57b61a00bb45b302c70de331
sakthive M <109778366+Sakthivel7413@users.noreply.github.com>
2024-09-16 18:08:22 +0530

    Add files via upload

---
+++ CallClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly_CSharp
+{
+    class CallClass
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine(Static.StaticMethod());
+        }
+
+    }
+}
